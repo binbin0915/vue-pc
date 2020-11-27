a6ea17e53a309867f0ca6f40b5ec8096d7be7113
zhaoyanga <1659200657@qq.com>
2020-11-27 16:20:43 +0800

    feat:"路由配置"

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,23 @@
 <template>
   <div>
-    <p>APP</p>
+    <Header />
+    <!-- 加载显示当前路由组件 -->
+    <router-view></router-view>
+    <Footer />
   </div>
 </template>
 
 <script>
-
+import Header from './components/Header'
+import Footer from './components/Footer'
 export default {
   name: 'App',
+  components:{
+    Header,
+    Footer
+  }
 }
 </script>
 
-<style>
+<style lang="less" scoped>
 </style>
---
+++ src/components/Header/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="header">
+    header...
+    <p>header children..</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style lang="less" scoped>
+.header {
+    background-color: yellowgreen;
+    p{
+      color:green
+    }
+}
+</style>
